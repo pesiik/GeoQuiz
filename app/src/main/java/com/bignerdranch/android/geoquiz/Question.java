952df2a15f5e762a136b3ec4f9ecaf891f18a9c8
pesiik <pesiik@mail.ru>
2018-04-19 17:15:51 +0400

    Done all tasks for 2 part.

--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -4,10 +4,12 @@ public class Question {
 
     private int mTextResId;
     private boolean mAnswerTrue;
+    private boolean mQuizResolved;
 
     public Question(int mTextResId, boolean mAnswerTrue) {
         this.mTextResId = mTextResId;
         this.mAnswerTrue = mAnswerTrue;
+        this.mQuizResolved = false;
     }
 
     public int getTextResId() {
@@ -24,6 +26,15 @@ public class Question {
 
     public void setAnswerTrue(boolean answerTrue) {
         mAnswerTrue = answerTrue;
+    }
+
+
+    public boolean isQuizResolved() {
+        return mQuizResolved;
+    }
+
+    public void setQuizResolved(boolean quizResolved) {
+        mQuizResolved = quizResolved;
     }
 }
 
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,13 +1,18 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -16,6 +21,9 @@ public class QuizActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
     private TextView mQuestionTextView;
+
+    private  List<Question> mAllResolvedAnswers = new ArrayList<>();
+    private List<Question> mRightAnswers = new ArrayList<>();
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_australia, true),
@@ -28,10 +36,15 @@ public class QuizActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        Log.d(TAG, "onCreate(Bundle) called");
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -39,6 +52,11 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton = findViewById(R.id.false_button);
         mNextButton = findViewById(R.id.next_button);
         mPrevButton = findViewById(R.id.prev_button);
+
+        if(savedInstanceState != null)
+        {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
+        }
 
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +92,8 @@ public class QuizActivity extends AppCompatActivity {
                 prevQuestion();
             }
         });
+
+
         updateQuestion();
     }
 
@@ -82,28 +102,97 @@ public class QuizActivity extends AppCompatActivity {
         mQuestionTextView.setText(question);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @SuppressLint("DefaultLocale")
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        if(!mQuestionBank[mCurrentIndex].isQuizResolved()){
+            mAllResolvedAnswers.add(mQuestionBank[mCurrentIndex]);
+        }
+        mQuestionBank[mCurrentIndex].setQuizResolved(true);
+
+        if(mQuestionBank.length == mAllResolvedAnswers.size()){
+            double result = (double) mRightAnswers.size() * 100d / (double) mAllResolvedAnswers.size();
+            Toast.makeText(this, String.format("You resolved all quizzes with result: %1.2f%%", result), Toast.LENGTH_LONG).show();
+        }
 
         int messageResId = 0;
 
         if(userPressedTrue == answerIsTrue){
             messageResId = R.string.correct_toast;
+            mRightAnswers.add(mQuestionBank[mCurrentIndex]);
         }
         else {
             messageResId = R.string.incorrect_toast;
         }
 
+        checkAnswerResolved();
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
     private void nextQuestion(){
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
         updateQuestion();
+        checkAnswerResolved();
     }
 
     private void prevQuestion(){
         mCurrentIndex = mCurrentIndex > 0 ? mCurrentIndex - 1 : mQuestionBank.length - 1;
         updateQuestion();
+        checkAnswerResolved();
+    }
+
+    private void checkAnswerResolved(){
+        if(mQuestionBank[mCurrentIndex].isQuizResolved())
+        {
+            mTrueButton.setVisibility(View.GONE);
+            mFalseButton.setVisibility(View.GONE);
+        }
+        else{
+            mTrueButton.setVisibility(View.VISIBLE);
+            mFalseButton.setVisibility(View.VISIBLE);
+        }
     }
 }
